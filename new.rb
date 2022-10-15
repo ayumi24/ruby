820dce9633264d511eab36433599286a135fd523
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-10-15 03:58:55 +0000

    [Finish]クラスについて　概念

--- new.rb
+++ new.rb
@@ -1,4 +1,9 @@
 class Car
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
@@ -9,10 +14,7 @@ class Car
 end
 
 car = Car.new
-car.turn("右")
-
-car = Car.new
-car.run(5)
+car.move("右",5)
 
 class Car
   def self.run(distance)
@@ -21,3 +23,10 @@ class Car
 end
 
 Car.run(10)
+
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+Car.turn("右")
